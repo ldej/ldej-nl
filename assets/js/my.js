3b83bc8601004efa534088ffa45451d0dc2c186c
Laurence de Jong <ldejong@xebia.com>
2020-07-13 17:56:03 +0530

    Update GA config

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded",
 (function(){
     var c = document.getElementById('logo');
-    console.log(c);
     function addAnim() {
         c.classList.add('logo-animated')
         // remove the listener, no longer needed
         c.removeEventListener('mouseover', addAnim);
-    };
+    }
 
     // listen to mouseover for the container
     c.addEventListener('mouseover', addAnim);
